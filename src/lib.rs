778a0c4fc3b22b34dec32d36b1592b9e21a2987e
Jessa <git@jessa0.com>
2021-01-28 10:46:56 -0800

    make example in crate-level docs easier to understand

--- src/lib.rs
+++ src/lib.rs
@@ -42,60 +42,65 @@
 //! ```
 //! use raft::log::mem::RaftLogMemory;
 //! use raft::node::{RaftConfig, RaftNode};
-//! use raft::message::RaftMessageDestination;
+//! use raft::message::{RaftMessageDestination, SendableRaftMessage};
 //! use rand_chacha::ChaChaRng;
 //! use rand_core::SeedableRng;
 //! use std::collections::VecDeque;
 //! use std::str;
 //!
 //! // Construct 5 Raft peers
-//! let config = RaftConfig {
-//!     election_timeout_ticks: 10,
-//!     heartbeat_interval_ticks: 1,
-//!     replication_chunk_size: usize::max_value(),
+//! type NodeId = usize;
+//! let mut peers = (0..5).map(|id: NodeId| RaftNode::new(
+//!     id,
+//!     (0..5).collect(),
+//!     RaftLogMemory::new_unbounded(),
+//!     ChaChaRng::seed_from_u64(id as u64),
+//!     RaftConfig {
+//!         election_timeout_ticks: 10,
+//!         heartbeat_interval_ticks: 1,
+//!         replication_chunk_size: usize::max_value(),
+//!     },
+//! )).collect::<Vec<_>>();
+//!
+//! // Simulate reliably sending messages instantaneously between peers
+//! let mut inboxes = vec![VecDeque::new(); peers.len()];
+//! let send_message = |src_id: NodeId, sendable: SendableRaftMessage<NodeId>, inboxes: &mut Vec<VecDeque<_>>| {
+//!     match sendable.dest {
+//!         RaftMessageDestination::Broadcast => {
+//!             println!("peer {} -> all: {}", src_id, &sendable.message);
+//!             inboxes.iter_mut().for_each(|inbox| inbox.push_back((src_id, sendable.message.clone())))
+//!         }
+//!         RaftMessageDestination::To(dst_id) => {
+//!             println!("peer {} -> peer {}: {}", src_id, dst_id, &sendable.message);
+//!             inboxes[dst_id].push_back((src_id, sendable.message));
+//!         }
+//!     }
 //! };
-//! let mut peers = (0..5)
-//!     .map(|id| RaftNode::new(id, (0..5).collect(),
-//!                             RaftLogMemory::new_unbounded(),
-//!                             ChaChaRng::seed_from_u64(id as u64),
-//!                             config.clone()))
-//!     .collect::<Vec<_>>();
 //!
 //! // Loop until a log entry is committed on all peers
 //! let mut appended = false;
 //! let mut peers_committed = vec![false; peers.len()];
-//! let mut messages = VecDeque::new();
 //! while !peers_committed.iter().all(|seen| *seen) {
-//!     // Tick the timer on each peer
 //!     for (peer_id, peer) in peers.iter_mut().enumerate() {
+//!         // Tick the timer
 //!         let new_messages = peer.timer_tick();
-//!         messages.extend(new_messages.map(|message| (peer_id, message)));
-//!     }
+//!         new_messages.for_each(|message| send_message(peer_id, message, &mut inboxes));
 //!
-//!     // Append a log entry on the leader
-//!     for (peer_id, peer) in peers.iter_mut().enumerate() {
+//!         // Append a log entry on the leader
 //!         if !appended && peer.is_leader() {
 //!             if let Ok(new_messages) = peer.append("Hello world!") {
-//!                 messages.extend(new_messages.map(|message| (peer_id, message)));
+//!                 new_messages.for_each(|message| send_message(peer_id, message, &mut inboxes));
 //!                 appended = true;
 //!             }
 //!         }
-//!     }
 //!
-//!     // Deliver messages between peers
-//!     while let Some((src_id, sendable)) = messages.pop_front() {
-//!         let dst_ids = match sendable.dest {
-//!             RaftMessageDestination::Broadcast  => 0..=peers.len() - 1,
-//!             RaftMessageDestination::To(dst_id) => dst_id..=dst_id,
-//!         };
-//!         for (peer_id, peer) in dst_ids.clone().zip(&mut peers[dst_ids]) {
-//!             let new_messages = peer.receive(sendable.message.clone(), src_id);
-//!             messages.extend(new_messages.map(|message| (peer_id, message)));
+//!         // Process message inbox
+//!         while let Some((src_id, message)) = inboxes[peer_id].pop_front() {
+//!             let new_messages = peer.receive(message, src_id);
+//!             new_messages.for_each(|message| send_message(peer_id, message, &mut inboxes));
 //!         }
-//!     }
 //!
-//!     // Check for committed log entries
-//!     for (peer_id, peer) in peers.iter_mut().enumerate() {
+//!         // Check for committed log entries
 //!         for log_entry in peer.take_committed() {
 //!             if !log_entry.data.is_empty() {
 //!                 println!("peer {} saw commit {}", peer_id, str::from_utf8(&log_entry.data).unwrap());
